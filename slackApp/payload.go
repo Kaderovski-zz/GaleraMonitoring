3e7e6c3200aea6776496e160720662b05572700d
F00b4rch <f00b4rch@gmail.com>
2017-06-20 11:44:35 +0200

    creating slack func

--- slackApp/payload.go
+++ slackApp/payload.go
@@ -1,1 +1,24 @@
 package slackApp
+
+import (
+	"fmt"
+ "github.com/ashwanthkumar/slack-go-webhook"
+)
+
+// Create an app in slack API and add an Incoming Webhoosk
+// more informations : https://api.slack.com/apps/
+func PayloadSlack(text string) {
+
+	// Add here your webhookurl
+	webhookUrl := "https://hooks.slack.com/services/your_key"
+
+	payload := slack.Payload{
+		Text:    text,
+		Username: "Galera_alerte",
+		Channel:  "#infra",
+	}
+	err := slack.Send(webhookUrl, "", payload)
+	if len(err) > 0 {
+		fmt.Printf("error : %s\n", err)
+	}
+}
